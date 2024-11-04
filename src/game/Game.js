987cab6231f513c4534d49f0278fb63a84aa653e
tigagit <github@yogesmagoges.com>
2024-11-05 03:34:03 +0800

    Update Game.js

--- src/game/Game.js
+++ src/game/Game.js
@@ -7,6 +7,11 @@ const MyGame = {
 
     moves: {
         addPlayer(G, ctx) {
+            console.log("Current state of G:", G); // Log G to see if players exist
+            if (!G.players) {
+                console.log("G.players is undefined, initializing now");
+                G.players = [];
+            }
             const newPlayerID = `Player ${G.players.length + 1}`;
             console.log("addPlayer move called, adding:", newPlayerID);
             G.players.push(newPlayerID);
