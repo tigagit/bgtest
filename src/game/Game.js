278c4a702a61f570b66d80688ec04c951a018783
tigagit <github@yogesmagoges.com>
2024-11-05 03:28:42 +0800

    Update Game.js

--- src/game/Game.js
+++ src/game/Game.js
@@ -5,7 +5,9 @@ const MyGame = {
     moves: {
         addPlayer(G, ctx) {
             const newPlayerID = `Player ${G.players.length + 1}`;
+            console.log("addPlayer move called, adding:", newPlayerID);
             G.players.push(newPlayerID);
+            console.log("Updated players list:", G.players);
         },
     },
 };
